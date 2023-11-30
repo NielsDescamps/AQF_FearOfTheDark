6fe752fad954d2e9216e8f070bbe22dee172bc84
NielsDescamps <niels.descampsduyck@student.kuleuven.be>
2023-11-30 17:05:31 +0100

    Update data_clean_up.py
    working filtering and adding columns necessary for pricing

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,13 +1,26 @@
 import pandas as pd
 
-def get_dfs_per_expiration_date(df,expiration_dates,price_dates,ticker):
+def filter_data(data,expiration_date,price_date,ticker):
     """
-    Returns a didctionary containing one or more dataframes of option data related to a given ticker
+    Returns a dataframe with filtered data
 
-    Input:
-    - base dataframe containing the entire dataset 
-    - 
+    Output columns:
+        - 'PriceDate': date of the price
+        - 'ExpirationDate'
+        - 'Type' : C=call; P=Put
+        - 'Strike'
+        - 'Bid'
+        - 'Ask'
+        - 'ImpliedVol'
+        - 'Ticker'
+        - 'Company'
+        - 'EorA'
+
+    Input: dataframe containing the full dataset
+ 
     """
+
+    df = data.copy()
 
     # 1. Filter out unnecessary columns and rename others
     columns_to_retain = ['The Date of this Price',
@@ -21,7 +34,6 @@ def get_dfs_per_expiration_date(df,expiration_dates,price_dates,ticker):
         'Description of the Issuing Company',
         '(A)merican, (E)uropean, or ?']
     df = df[columns_to_retain]
-    
     df['Strike Price of the Option Times 1000'] = df['Strike Price of the Option Times 1000']/1000
     
     new_column_names = ['PriceDate',
@@ -40,47 +52,35 @@ def get_dfs_per_expiration_date(df,expiration_dates,price_dates,ticker):
     for column in date_columns:
         df[column] = df[column].astype(str)
     
-    filtered_df1 = pd.DataFrame()
-    for specific_date in price_dates:
-        date_filter = df['PriceDate'] == specific_date
-        filtered_df1 = pd.concat([filtered_df1, df[date_filter]])
-    
-    # print('df before filtering on expiration_date',filtered_df1)
-    
-    dfs = dict.fromkeys(expiration_dates,None)
-    
-    for expiration_date in expiration_dates:
-        date_filter = filtered_df1['ExpirationDate'] == expiration_date
-        filtered_df = pd.DataFrame()
-        filtered_df = pd.concat([filtered_df, filtered_df1[date_filter]])
 
-        filtered_df['PriceDate'] = pd.to_datetime(filtered_df['PriceDate'], format='%d.%m.%Y')
-        filtered_df['ExpirationDate'] = pd.to_datetime(filtered_df['ExpirationDate'], format='%d.%m.%Y')
+    # 2. Filter for the date of pricing, expiration date and ticker
+    df = df[df['PriceDate'] == price_date]
+    df = df[df['ExpirationDate'] == expiration_date]
+    df = df[df['Ticker'] == ticker]
 
-        filtered_df = filtered_df[filtered_df['Ticker'] == ticker]
-        dfs[expiration_date] = filtered_df
-        # print('filtered df: ',filtered_df)
+    # 3. Convert dates to datetime objects
+    df['PriceDate'] = pd.to_datetime(df['PriceDate'], format='%d.%m.%Y')
+    df['ExpirationDate'] = pd.to_datetime(df['ExpirationDate'], format='%d.%m.%Y')
     
-    return dfs
+    return df
 
-def calc_params(dfs,exp_date,S0,r,q):
+def calc_params(df,S0,r,q):
     """
     Takes dataframe containing option data for one price date and expiration date 
     - calculate the Midquote and store in seperate column
     - calculate time to maturity [years - 250 days]
+    - remove duplicates wrt strike, TTM and type
 
-    Returns reduced dataframe with only relevant columns for calibration:
-    - Stock price at time 0: S0
-    - interest rate: r
-    - dividend yield: q
-    - strike price: K
-    - time to maturity: TTM
-    - market price (midquote): MQ
+    Returns dataframe with only relevant columns for calibration:
+    - Stock price at time 0: 'S0'
+    - interest rate: 'R'
+    - dividend yield: 'Q'
+    - strike price: 'Strike'
+    - time-to-maturity: 'TTM' (expressed in years assuming 252 trading days per year)
+    - market price (midquote): 'MQ'
     
     """
-    df = dfs[exp_date]
     df_out = df.copy()
-    print(df_out)
     
     # 1. Calculate time to maturity TTM
     df_out['TTM'] = (df['ExpirationDate'] - df['PriceDate']).dt.days
@@ -89,24 +89,29 @@ def calc_params(dfs,exp_date,S0,r,q):
     # 2. Calculate Miquote
     df_out['MQ'] = (df_out['Ask'] + df_out['Bid'])/2
     
-    
     # 3. Filter for unique combinations of strike and maturity 
     df_out = df_out.drop_duplicates(subset=['Strike','TTM','Type'], keep='last')
-    
-    print(df_out.shape)
+
+    # 4. Add interest rate, dividend yield and initial index price to the dataframe
+    df_out['R'] = r
+    df_out['Q'] = q
+    df_out['S0'] = S0
     
     return df_out
 
 
 data = pd.read_csv('data_XNG/data_options.csv')
-exp_dates = ['16.01.2010', '17.04.2010']
-exp_date = '16.01.2010'
-price_dates = ['04.01.2010']
-ticker = 'XNG'
 
-dfs = get_dfs_per_expiration_date(data,exp_dates,price_dates,ticker)
-df_calc = calc_params(dfs,exp_date,1,1,1)
+exp_date = '16.01.2010' # or  '17.04.2010'
+price_date = '04.01.2010'
+ticker = 'XNG' #or BTK, XBD and MSH (use XNG, others have less quotes per day)
+df = filter_data(data,exp_date,price_date,ticker)
+
+S0 = 100
+r=0.05
+q=0.02
+df_calc = calc_params(df,S0,r,q)
 print(df_calc)
-print('output: ',df_calc.shape)
+print('output shape: ',df_calc.shape)
 
 
