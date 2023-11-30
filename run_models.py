8336cb48b5cbd8d106e3a9b907b99ba3cfd930bf
Marcus Möller <marcusmoller@Marcuss-MacBook-Pro-2.local>
2023-11-30 17:46:28 +0100

    added pdf plot half working

--- run_models.py
+++ run_models.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from src.jump_characteristic_function import mutualjump_characteristic_function
 from src.jump_characteristic_function import joint_characteristic_function
 from src.heston_characteristic import heston_characteristic
+from src.characteristic_to_pdf import characteristic_to_pdf
 from src.jump_characteristic_function import lewis_pricing_formula
 from scipy.fft import fft, rfft
 from scipy.fft import fftfreq, rfftfreq
@@ -133,50 +134,14 @@ def run_models():
     pdf_joint = np.fft.ifftshift(np.fft.ifft(PHI_joint))
     #pdf_joint = np.fft.ifftshift(np.fft.ifft(PHI_joint))
 
-    # b is the endvalue of which u is evaluated in the char func integral
-    b = 100
-    N = 1000
-    u, delta_u = np.linspace(-b, b, N, retstep=True)
-
-    #Create the frequency domain axis
-    beta = N * np.pi / (2*b)
-    z = np.linspace(-beta, beta, N)
-    alpha = np.exp(1j*b*z)*char_func(u-b-delta_u)*delta_u
-    pdf = np.abs(1/(2*np.pi)*np.fft.fft(alpha))**2
-    plt.figure()
-    plt.plot(z, pdf, label="from characteristic")
-    plt.plot(z, norm.pdf(z), label="norm pdf")
-    plt.plot(u, char_func(u), label = "characteristic")
-    plt.legend()
-    plt.xlabel('Frequency[Hz]')
-    plt.ylabel('Amplitude')
-    plt.title('Spectrum')
-    plt.show()
-
-
-
-    ## Implementation of correct fourier transform - doesnt work
-    sample_rate = 1/(u[1]-u[0])
-    N = len(u)
-    normalize = N / 2
-    frequency_axis = fftfreq(N, d=1.0 / sample_rate)
-    fourier = fft(char_func(u))
-    norm_amplitude = np.abs(fourier) / normalize
-    plt.figure()
-    plt.plot(frequency_axis, norm_amplitude, label = "from characteristic")
-    plt.plot(frequency_axis, norm.pdf(frequency_axis), label = "norm pdf")
-    plt.legend()
-    plt.xlabel('Frequency[Hz]')
-    plt.ylabel('Amplitude')
-    plt.title('Spectrum')
-    plt.show()
-
+    k_heston, norm_heston = characteristic_to_pdf(u,np.abs(pdf_heston))
+    k_joint, norm_joint = characteristic_to_pdf(u, np.abs(pdf_joint))
 
     plt.figure()
     #plt.plot(u_values, np.real(characteristic_function), label='standard normal pdf')
     #plt.plot(u_values, np.imag(characteristic_function), label = 'standard imaginary')
-    plt.plot(k, np.abs(pdf_joint), label = 'pdf joint')
-    plt.plot(k, np.abs(pdf_heston), label='pdf heston')
+    plt.plot(k_heston, norm_heston, label = 'pdf joint')
+    plt.plot(k_joint, norm_joint, label='pdf heston')
     #plt.plot(u, np.real(PHI_jump, label = 'jump abs')
     #plt.plot(k, np.real(pdf_joint), label='real norm')
     # plt.plot(u_values, np.imag(pdf), label = 'pdf imag')
@@ -186,6 +151,76 @@ def run_models():
     plt.legend()
     plt.show()
 
+def plot_pdf():
+    # Set the jump distribution
+    jump_distribution = "Gaussian"
+    # Select index to model
+    index = 1
+    #Eval time
+    t = 0
+    # Maturity
+    T = 1
+    # time step for the evaluation of the set of ODEs
+    h = 0.1
+    # Import data
+    heston_params, heston_config, jump_process_params, jump_process_config, option_data = import_model_data()
+    kappa, eta, theta, rho, sigma0, r, q, S0 = heston_params['kappa'], heston_params['eta'], heston_params['theta'], \
+    heston_params['rho'], heston_params['v0'], heston_config['r'], heston_config['q'], heston_config['last_close']
 
-run_models()
+    if jump_distribution == "Exponential":
+        jump_params = [np.array(jump_process_params['alpha']),
+                       np.array(jump_process_params['delta']),
+                       np.array(jump_process_params['gamma']),
+                       np.array(jump_process_params['lambda_bar'])
+                       ]
+    elif jump_distribution == "Gaussian":
+        jump_params = [np.array(jump_process_params['alpha']),
+                       np.array(jump_process_params['delta']),
+                       np.array(jump_process_params['beta']),
+                       np.array(jump_process_params['sigma']),
+                       np.array(jump_process_params['lambda_bar'])
+                       ]
+
+        N = 4096
+        alpha = 1.5
+        eta_grid = 0.25
+        lambda_val = 2 * np.pi / (N * eta_grid)
+        b = lambda_val * N / 2
+        k = np.arange(-b, b, lambda_val)
+        v = np.arange(-N * eta_grid / 2, N * eta_grid / 2, eta_grid)
+        u = np.array(v)
+        lambda_zero = jump_params[3]
+
+        PHI_joint = [joint_characteristic_function(val, t, T, heston_params['kappa'], heston_params['eta'],
+                                                   heston_params['theta'],
+                                                   heston_params['rho'], heston_params['v0'], heston_config['r'],
+                                                   heston_config['q'],
+                                                   heston_config['last_close'], jump_params, lambda_zero, h, index,
+                                                   jump_distribution) for val in u]
+        PHI_heston = [heston_characteristic(kappa, eta, theta, rho, sigma0, r, q, S0, T, val) for val in u]
+
+        pdf_heston = np.abs(np.fft.ifftshift(np.fft.ifft(PHI_heston)))
+        pdf_joint = np.abs(np.fft.ifftshift(np.fft.ifft(PHI_joint)))
+
+        # Normalisation
+        pdf_heston = pdf_heston / lambda_val
+        pdf_joint = pdf_joint / lambda_val
+        #Translation
+        #TODO this is sloppy dont see where the need for translation comes from
+        E_h = np.sum(k * pdf_heston * lambda_val)
+        E_j = np.sum(k * pdf_joint * lambda_val)
+        k_heston = k - E_h
+        k_joint = k - E_h
+
+        plt.figure()
+        plt.plot(k_heston, pdf_heston, label='pdf Hawkes Model')
+        plt.plot(k_joint, pdf_joint, label='pdf Heston Model')
+        plt.xlabel('z [log-returns]')
+        plt.ylabel('probability density')
+        plt.title('Probability density functions')
+        plt.legend()
+        plt.show()
+
+
+
 
