c49f2bbc6c8397d59ca5a5625f9fd95f358729a0
NielsDescamps <niels.descampsduyck@student.kuleuven.be>
2023-12-03 20:08:10 +0100

    remove legacy code

--- src/get_maturities.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def get_maturities(expiry_dates, date_of_retrieval):
-    exp_dates = [datetime.strptime(date, '%Y-%m-%d') for date in expiry_dates]
-    retrieval_date = datetime.strptime(date_of_retrieval, '%Y-%m-%d')
-    T_datetime = [(date - retrieval_date).days for date in exp_dates]
-    T = [days / 365 for days in T_datetime]
-    return T
